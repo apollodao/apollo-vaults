2569972fa9757e0c3d62d06a0720a31896d3993b
Pacman <pacman@apollo.farm>
2023-02-17 13:47:55 +0100

    refactor(test): move robot into test_helpers mod

---
+++ contracts/osmosis-vault/tests/test_helpers/constants.rs
@@ -0,0 +1,7 @@
+pub(crate) const UOSMO: &str = "uosmo";
+pub(crate) const UATOM: &str = "uatom";
+pub(crate) const UION: &str = "uion";
+pub(crate) const STAKE: &str = "stake";
+pub(crate) const INITIAL_BALANCE: u128 = 100_000_000_000_000;
+pub(crate) const TEST_CONFIG_PATH: &str = "tests/configs/osmosis.yaml";
+pub(crate) const TWO_WEEKS_IN_SECONDS: u64 = 60 * 60 * 24 * 14;
